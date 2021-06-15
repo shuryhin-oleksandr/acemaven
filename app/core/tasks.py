f4535f41f337a00caf1285c7ee822f37ce882e6d
Arseniy Boiko <a.boiko@nord-soft.com>
2021-06-15 18:07:58 +0300

    email

--- app/core/tasks.py
+++ app/core/tasks.py
@@ -1,4 +1,6 @@
 import logging
+import smtplib
+
 from django.template.loader import get_template
 
 from config.celery import celery_app
@@ -10,11 +12,18 @@ from config.settings.local import DOMAIN_ADDRESS
 
 from django.utils.translation import ugettext as _
 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 logger = logging.getLogger("acemaven.task.logging")
 
 
 @celery_app.task
 def send_registration_email(token, recipient_email, role):
+    msg = MIMEMultipart('alternative')
+    msg['From'] = settings.EMAIL_HOST_USER
+    msg['To'] = recipient_email
+
     subject = _('Acemaven. Registration process.')
     logger.info(f'New registration email is going to be send to {recipient_email}')
 
@@ -24,15 +33,31 @@ def send_registration_email(token, recipient_email, role):
         message_body = f'{DOMAIN_ADDRESS}additional/user?token={token}'
     template_html = get_template(f"core/emails_templates/index.html")
     text = _("To complete your sign-up, please press the button:")
+    body_text = text
     context = {
         "email": recipient_email,
         "text": text,
         "link": message_body,
         }
     message_html = template_html.render(context)
+
+    part1 = MIMEText(body_text, 'plain')
+    part2 = MIMEText(message_html, 'html')
+
+    msg.attach(part1)
+    msg.attach(part2)
+
+    mail = smtplib.SMTP("smtp.outlook.office365.com", 587, timeout=20)
+    mail.starttls()
+    recepient = [recipient_email, ]
+
+    mail.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
+
     logger.debug(f"sending invitation email to {recipient_email}")
-    send_mail(subject, message_body, settings.EMAIL_HOST_USER, [recipient_email], html_message=message_html)
+    mail.sendmail((settings.EMAIL_HOST_USER, recepient, msg.as_string()))
+    # send_mail(subject, message_body, settings.EMAIL_HOST_USER, [recipient_email], html_message=message_html)
     logger.info(f"invitation has been sent to {recipient_email} from {settings.EMAIL_HOST_USER}")
+    mail.quit()
 
 
 @celery_app.task
